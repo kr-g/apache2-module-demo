ff3dd3f73b141072e983a79afb70fbe40378a954
kr-g <kr-g>
2016-11-29 11:09:48 +0100

    refactored to reduce size of docker image

--- docker-demo/setup.sh
+++ docker-demo/setup.sh
@@ -1,18 +1,10 @@
 
-# get the main repo 
-
-cd ~/repo
-
-git clone https://github.com/kr-g/apache2-module-demo.git
-
-
 # build the markdown apache handler
 
-cd apache2-module-demo/asdbTest/
+cd ~/repo/apache2-module-demo/asdbTest/
 
 sudo apxs -c -i -a mod_asdbTest.c
 
-sudo make all 
 
 
 # build the javascript apache handler
@@ -21,21 +13,15 @@ cd ~/repo/apache2-module-demo/asdbjs/
 
 sudo apxs -c -i -a mod_asdbjs.c
 
-sudo make all 
 
 
-# get and build spidermonkey
+# build spidermonkey 
 
-cd ~/repo/
-
-git clone https://github.com/mozilla/gecko-dev.git --branch GECKO4502_2016040719_RELBRANCH --single-branch
-
-cd gecko-dev/js/src
+cd ~/repo/gecko-dev/js/src
 
 autoconf2.13
 
 mkdir build_OPT.OBJ
-
 cd build_OPT.OBJ
 
 ../configure
@@ -43,10 +29,8 @@ cd build_OPT.OBJ
 make
 
 cd dist/bin
-
 # use link in case node.js is in the path
 ln -s js spidermonkeyjs 
-
 
 
 
--- docker-demo/startup.sh
+++ docker-demo/startup.sh
@@ -2,7 +2,7 @@
 
 . /etc/apache2/envvars
 
-export PATH=$PATH:~/repo/gecko-dev/js/src/build_OPT.OBJ/dist/bin/
+export PATH=$PATH:~/repo/spidermonkey
 
 #  start in debug mod 
 apache2 -X
